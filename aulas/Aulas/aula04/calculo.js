14e2835ebf69a546776b91ce30cda4e752ea0088
Grangola <tiago.rios713@gmail.com>
2024-09-16 18:32:47 -0300

    correção e console js

--- aulas/Aulas/aula04/calculo.js
+++ aulas/Aulas/aula04/calculo.js
@@ -1,6 +1,6 @@
 let proximoId = 1
 
-function adiocionarLinha(){
+function adicionarLinha() {
     //entrada de valores do formlário
     const nome = document.getElementById("nome").value;
     const idade = document.getElementById("idade").value;
@@ -8,32 +8,34 @@ function adiocionarLinha(){
     const demissao = document.getElementById("demissao").value;
 
     //se for preenchido vazio
-    if(nome === '' || idade === '' || admissao === ''){
+    if (nome === '' || idade === '' || admissao === '') {
         alert("Preencha os valores do formulário!")
+    } else {
+        //cria uma linha se não existir
+        const tabela = document.getElementById("tabelaDados").getElementsByTagName('tbody')[0];
+
+        //inserindo uma nova linha
+        const novaLinha = tabela.insertRow();
+
+        //inserindo os valores da linha
+        const celId = novaLinha.insertCell(0);
+        const celNome = novaLinha.insertCell(1);
+        const celIdade = novaLinha.insertCell(2);
+        const celAdmissao = novaLinha.insertCell(3);
+        const celDemissao = novaLinha.insertCell(4);
+
+        //inserindo os valores dentro das celulas
+        celId.innerHTML = proximoId;
+        celNome.innerHTML = nome;
+        celIdade.innerHTML = idade;
+        celAdmissao.innerHTML = admissao;
+        celDemissao.innerHTML = demissao;
+
+        proximoId++
+
+        //limpar formulário
+        document.getElementById("linhaForm").reset();
     }
 
-    //cria uma linha se não existir
-    const tabela = document.getElementById("tabelaDados").getElementsByTagName('tbody')[0];
 
-    //inserindo uma nova linha
-    constnovaLinha = tabela.insertRow;
-
-    //inserindo os valores da linha
-    const celId = novaLinha.insertCell(0);
-    const celNome = novaLinha.insertCell(1);
-    const celIdade = novaLinha.insertCell(2);
-    const celAdmissao = novaLinha.insertCell(3);
-    const celDemissao = novaLinha.insertCell(4);
-
-    //inserindo os valores dentro das celulas
-    CellId.innerHTML = proximoId;
-    cellNome.innerHTML = nome;
-    cellIdade.innerHTML = idade;
-    cellAdmissao.innerHTML = admissao;
-    cellDemissao.innerHTML = demissao;
-
-    proximoI++
-
-    //limpar formulário
-    document.getElementById("linhaForm").reset ();
 }
